b8e045a4f21b79c1b9b669ec4fce180ec7414165
Vishal Lakshmi <vishal.rox26@gmail.com>
2020-11-17 19:55:49 -0800

    Initial Commit

---
+++ NetlixClone/NetlixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetlixCloneApp.swift
+//  NetlixClone
+//
+//  Created by Vishal Lakshminarayanappa on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct NetlixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
